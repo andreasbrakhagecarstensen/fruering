ca06cb8d92b33aedf29485580c2b4bfd21ed0e18
Andreas Brakhage Carstensen <dev@acarstensen.com>
2018-03-11 14:03:11 +0100

    Updated base settings with localization changes

--- fruering/settings/base.py
+++ fruering/settings/base.py
@@ -119,9 +119,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'da-dk'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'GMT+1'
 
 USE_I18N = True
 
