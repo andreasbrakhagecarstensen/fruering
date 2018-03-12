9779e66c13e9412e58a9f0536411da904a76a091
Andreas Brakhage Carstensen <dev@acarstensen.com>
2018-03-12 22:03:46 +0100

    Added blogpages context variable with the blog pages in reverse chron. order

--- blog/models.py
+++ blog/models.py
@@ -7,6 +7,12 @@ from wagtail.core.models import Page
 class BlogIndexPage(Page):
     intro = RichTextField(blank=True)
 
+    def get_context(self, request, *args, **kwargs):
+        context = super().get_context(request)
+        blogpages = self.get_children().live().order_by('-first_published_at')
+        context['blogpages'] = blogpages
+        return context
+
     content_panels = Page.content_panels + [
         FieldPanel('intro', classname="full")
     ]
