c1919183bc0b7192f54a69854322c4cc7f149d6f
Andreas Brakhage Carstensen <dev@acarstensen.com>
2018-03-18 12:10:23 +0100

    fix homepage url added method for getting the homepage for current site

--- core/models.py
+++ core/models.py
@@ -9,9 +9,11 @@ class BasePage(Page):
         context = super().get_context(request)
         context['menu_items'] = self.menu_items
 
-        homepage = self.get_root()
-        context['homepage'] = homepage
+        context['homepage'] = self.get_site_homepage()
         return context
+
+    def get_site_homepage(self):
+        return self.get_site().root_page
 
     class Meta:
         abstract = True
