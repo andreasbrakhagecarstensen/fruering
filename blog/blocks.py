10517fdca7cd0a12efd012c548c4c9b2a91fb48e
Andreas Brakhage Carstensen <dev@acarstensen.com>
2018-05-07 21:47:54 +0200

    Add template to blog index page block
    #22

--- blog/blocks.py
+++ blog/blocks.py
@@ -1,13 +1,16 @@
 from wagtail.core import blocks
 from wagtail.core.blocks import StructBlock
 
-from .models import BlogPage
+from .models import BlogIndexPage
 
 
-class BlogPageBlock(StructBlock):
+class BlogIndexPageBlock(StructBlock):
     block_title = blocks.CharBlock(required=True, max_length=80)
-    blog_page = blocks.PageChooserBlock(target_model=BlogPage)
+    blog_page = blocks.PageChooserBlock(target_model=BlogIndexPage)
     post_count = blocks.IntegerBlock(required=False,
                                      help_text="Number of post shown in this block. Default is 5. Max is 10.",
                                      min_value=1,
                                      max_value=10)
+
+    class Meta:
+        template = 'blog/blocks/block_index_page_block.html'
