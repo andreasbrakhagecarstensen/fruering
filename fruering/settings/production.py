42f8d7f401217ba01bd1ca5d69f3569bdf13351f
Andreas Brakhage Carstensen <dev@acarstensen.com>
2018-05-13 13:42:19 +0200

    Force https on QA and production
    resolves #32

--- fruering/settings/production.py
+++ fruering/settings/production.py
@@ -2,6 +2,7 @@ from .base import *
 import django_heroku
 
 DEBUG = False
+SECURE_SSL_REDIRECT = True
 
 django_heroku.settings(locals())
 
--- fruering/settings/qa.py
+++ fruering/settings/qa.py
@@ -4,6 +4,7 @@ import django_heroku
 django_heroku.settings(locals(), allowed_hosts=False)
 
 DEBUG = True
+SECURE_SSL_REDIRECT = True
 ALLOWED_HOSTS = ['fruering-qa.herokuapp.com']
 
 try:
