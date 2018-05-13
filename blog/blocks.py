f16b28b03b539dbf58bb233ca0d02808cfcf9b09
Andreas Brakhage Carstensen <dev@acarstensen.com>
2018-05-13 13:49:19 +0200

    Remove count from blog index page block

--- blog/blocks.py
+++ blog/blocks.py
@@ -7,10 +7,7 @@ from .models import BlogIndexPage
 class BlogIndexPageBlock(StructBlock):
     block_title = blocks.CharBlock(required=True, max_length=80)
     blog_page = blocks.PageChooserBlock(target_model=BlogIndexPage)
-    post_count = blocks.IntegerBlock(required=False,
-                                     help_text="Number of post shown in this block. Default is 5. Max is 10.",
-                                     min_value=1,
-                                     max_value=10)
+
 
     def get_context(self, value, parent_context=None):
         context = super().get_context(value, parent_context)
