aa7a721e18dfda6172d4b2185ac2cb37cdc87bc9
Andreas Brakhage Carstensen <dev@acarstensen.com>
2018-05-16 21:35:47 +0200

    Add environment path in S3 and optional debug mode in qa

--- fruering/settings/production.py
+++ fruering/settings/production.py
@@ -4,6 +4,7 @@ DEBUG = False
 SECURE_SSL_REDIRECT = True
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_LOCATION = 'production'
 
 try:
     from .local import *
--- fruering/settings/qa.py
+++ fruering/settings/qa.py
@@ -3,10 +3,10 @@ import django_heroku
 
 django_heroku.settings(locals(), allowed_hosts=False)
 
-DEBUG = True
+DEBUG = os.getenv('IS_DEBUG', False)
 SECURE_SSL_REDIRECT = True
 ALLOWED_HOSTS = ['fruering-qa.herokuapp.com']
-
+AWS_LOCATION = 'qa'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 try:
