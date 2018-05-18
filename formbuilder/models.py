c8d74379ad432413493e1c4e45392b60cc1695e1
Andreas Brakhage Carstensen <dev@acarstensen.com>
2018-05-18 20:47:44 +0200

    Add FormField and FormEmailPage
    related issues #7, #21

--- formbuilder/models.py
+++ formbuilder/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from modelcluster.fields import ParentalKey
+from wagtail.admin.edit_handlers import FieldPanel, InlinePanel, MultiFieldPanel, FieldRowPanel
+from wagtail.contrib.forms.edit_handlers import FormSubmissionsPanel
+from wagtail.contrib.forms.models import AbstractFormField, AbstractEmailForm
+from wagtail.core.fields import RichTextField
 
-# Create your models here.
+
+class FormField(AbstractFormField):
+    page = ParentalKey('FormPage', on_delete=models.CASCADE, related_name='form_fields')
+
+class FormEmailPage(AbstractEmailForm):
+    intro = RichTextField(blank=True)
+    thank_you_text = RichTextField(blank=True)
+
+    content_panels = AbstractEmailForm.content_panels + [
+        FormSubmissionsPanel(),
+        FieldPanel('intro', classname='full'),
+        InlinePanel('form_fields', label='Form fields'),
+        FieldPanel('thank_you_text', classname='full'),
+        MultiFieldPanel([
+            FieldRowPanel([
+                FieldPanel('from_address', classname='col6'),
+                FieldPanel('to_address', classname='col6'),
+            ]),
+        ]),
+    ]
