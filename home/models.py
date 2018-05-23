56ba6a7683a5fd4d83b4b6482830dcb480879ad5
Andreas Brakhage Carstensen <dev@acarstensen.com>
2018-05-23 19:54:28 +0200

    Add richtext article block to home page and add article base template

--- home/models.py
+++ home/models.py
@@ -1,6 +1,7 @@
 from wagtail.core.models import Page
 from wagtail.core.fields import StreamField
 from wagtail.admin.edit_handlers import StreamFieldPanel
+from wagtail.core.blocks import CharBlock, RichTextBlock
 
 from blog import blocks
 
@@ -8,6 +9,12 @@ from blog import blocks
 class HomePage(Page):
     body = StreamField([
         ('blogroll', blocks.BlogIndexPageBlock()),
+        ('richtext_article', blocks.StructBlock(
+            [
+                ('header', CharBlock()),
+                ('paragraph', RichTextBlock()),
+            ],
+            template='fruering/blocks/richtext_article_block.html'))
     ])
 
     content_panels = Page.content_panels + [
