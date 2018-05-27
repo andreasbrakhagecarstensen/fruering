51ca810e5ed5e3fdf86d839fcd3bb42b987988d5
Andreas Brakhage Carstensen <dev@acarstensen.com>
2018-05-27 12:28:07 +0200

    Simplify templates to use new article structure
    resolves #61, #60, #59

--- fruering/models.py
+++ fruering/models.py
@@ -6,7 +6,7 @@ from wagtail.admin.edit_handlers import FieldPanel
 
 
 class RichTextPage(Page):
-    body = RichTextField(blank=True)
+    body = RichTextField(blank=True, features=['h2', 'h3', 'h4', 'bold', 'italic', 'ol', 'ul', 'hr', 'link'])
 
     content_panels = Page.content_panels + [
         FieldPanel('body', classname="full"),
