23f89fb0aaa96fae731ee1872105649c8ca4b355
Andreas Brakhage Carstensen <dev@acarstensen.com>
2019-05-31 21:23:42 +0200

    Fix wrong allowd hosts

--- fruering/settings/dev.py
+++ fruering/settings/dev.py
@@ -1,7 +1,7 @@
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('IS_DEBUG', True)
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '-^b0r7@*-^8t_s6l0svd50ep*177is8@6co8hrheu48lu_da5x'
--- fruering/settings/production.py
+++ fruering/settings/production.py
@@ -1,8 +1,8 @@
 from .base import *
 
-DEBUG = False
+DEBUG = os.getenv('IS_DEBUG', False)
 SECURE_SSL_REDIRECT = True
-ALLOWED_HOSTS = ['fruering.herokuapp.com', 'fruering.dk']
+ALLOWED_HOSTS = ['fruering.herokuapp.com', 'fruering.dk', 'www.fruering.dk']
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_LOCATION = 'production'
 
