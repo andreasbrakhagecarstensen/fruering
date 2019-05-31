4cef258a4d92126cfb4f239f1c39b0ae38c11318
Andreas Brakhage Carstensen <dev@acarstensen.com>
2019-05-31 15:46:05 +0200

    Update settings and remove dependency
    - Remove django_heroku dependency
    - Add related settings to base
    - Add local dependency on env vars SECRET_KEY and DATABASE_URL
    - Update production allowed hosts
    - Update qa allowed hosts and remove review app specifics
    
    closes #82

--- fruering/settings/production.py
+++ fruering/settings/production.py
@@ -2,7 +2,7 @@ from .base import *
 
 DEBUG = False
 SECURE_SSL_REDIRECT = True
-
+ALLOWED_HOSTS = ['fruering.herokuapp.com', 'fruering.dk']
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_LOCATION = 'production'
 
