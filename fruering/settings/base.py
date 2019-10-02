43b66d9cacd35c0fd114e43e0c46350688e55d20
Andreas Brakhage Carstensen <dev@acarstensen.com>
2019-10-02 13:17:04 +0200

    Update Wagtail and other dependencies

--- fruering/settings/base.py
+++ fruering/settings/base.py
@@ -103,7 +103,7 @@ WSGI_APPLICATION = 'fruering.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=(not DEBUG))
 }
 
 
--- fruering/settings/dev.py
+++ fruering/settings/dev.py
@@ -1,3 +1,8 @@
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
 from .base import *
 
+SECURE_SSL_REDIRECT = False
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
