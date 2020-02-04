5b3b9d6bb802964406ce1b46e773ba9dcbe478f3
Andreas Carstensen <dev@acarstensen.com>
2020-02-04 10:35:27 +0100

    Update base.py

--- fruering/settings/base.py
+++ fruering/settings/base.py
@@ -63,9 +63,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
